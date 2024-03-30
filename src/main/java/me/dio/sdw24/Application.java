d282a503150a3c35dff8a216436f031eafe34e74
Guilherme Nicolasi <guilherme_nicolasi@hotmail.com.br>
2024-03-30 20:55:29 -0300

    first commit

---
+++ src/main/java/me/dio/sdw24/Application.java
@@ -0,0 +1,21 @@
+package me.dio.sdw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import me.dio.sdw24.application.ListChampionsUseCase;
+import me.dio.sdw24.domain.model.ports.ChampionsRepository;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public ListChampionsUseCase provideListChampionsUseCase(ChampionsRepository championsRepository) {
+		return new ListChampionsUseCase(championsRepository);
+	}
+}
---
+++ src/main/java/me/dio/sdw24/adapters/out/ChampionsJdbcRepository.java
@@ -0,0 +1,41 @@
+package me.dio.sdw24.adapters.out;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import me.dio.sdw24.domain.model.Champions;
+import me.dio.sdw24.domain.model.ports.ChampionsRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ChampionsJdbcRepository implements ChampionsRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Champions> championsRowMapper;
+
+    public ChampionsJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.championsRowMapper = (rs, rowNum) -> new Champions(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getString("role"),
+            rs.getString("lore"),
+            rs.getString("image_url")
+        );
+    }
+
+    @Override
+    public List<Champions> findAll() {
+        return jdbcTemplate.query("SELECT * FROM CHAMPIONS", championsRowMapper);
+    }
+
+    @Override
+    public Optional<Champions> findById(Long id) {
+        String sql = "SELECT * FROM CHAMPIONS WHERE ID = ?";
+        Champions champion = jdbcTemplate.queryForObject(sql, championsRowMapper, id);
+        return Optional.ofNullable(champion);
+    }
+
+}
